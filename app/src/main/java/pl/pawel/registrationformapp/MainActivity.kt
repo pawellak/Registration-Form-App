e72a35daf4be859668e0914515075b174ce0203f
Paweł Łąk <p.lak@sygnisoft.com>
2025-04-23 15:53:54 +0200

    registration form app

---
+++ app/src/main/java/pl/pawel/registrationformapp/MainActivity.kt
@@ -0,0 +1,50 @@
+package pl.pawel.registrationformapp
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import pl.pawel.registrationformapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setContentView(R.layout.activity_main)
+        setupSpinner()
+        setupButton()
+    }
+
+    private fun setupButton() {
+        binding.buttonCreateAccount.setOnClickListener {
+            createAccount()
+        }
+    }
+
+    private fun createAccount() {
+
+        val user = User(
+            title = binding.spinnerTitle.selectedItem.toString(),
+            firstName = binding.editTextFirstName.text.toString(),
+            lastName = binding.editTextLastName.text.toString(),
+            email = binding.editTextEmail.text.toString(),
+            phone = binding.editTextPhoneNumber.text.toString(),
+            password = binding.editTextPassword.text.toString()
+        )
+
+
+
+    }
+
+    private fun setupSpinner() {
+        val titles = arrayOf("Miss", "Mrs", "Ms", "Mr", "Dr", "Prof")
+        val titleAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, titles)
+        binding.spinnerTitle.adapter = titleAdapter
+    }
+}
